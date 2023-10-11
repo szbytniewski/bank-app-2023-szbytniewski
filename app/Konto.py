c7ee4c7d313c23fdd9a54bf5740ff0963faf2760
Sebastian Zbytniewski <s.zbytniewski.624@studms.ug.edu.pl>
2023-10-11 17:22:32 +0200

    Added the first 4 features and working on the 5th

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,31 @@
 class Konto:
-    def __init__(self):
-        pass
+    def __init__(self,imie,nazwisko,pesel,promo_code = None):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.saldo = 0
+
+
+        if len(pesel) != 11:
+            self.pesel = "Niepoprawny pesel!"
+        else:
+            self.pesel = pesel
+
+        # if promo_code is not None:
+        #     if promo_code.startswith("PROM_") and len(promo_code) == 8:
+        #         self.saldo = 50
+        #     else:
+        #         self.saldo = 0
+        if self.is_promo_code_correct(promo_code):
+            self.saldo = 50
+        else:
+            self.saldo = 0
+
+            
+
+    def is_promo_code_correct(self, promo_code):
+        if promo_code is None:
+            return False
+        if promo_code.startswith("PROM_") and len(promo_code) == 8:
+            return True
+        else:
+            return False
