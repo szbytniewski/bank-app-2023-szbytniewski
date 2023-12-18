679a43916d70c3f5f9b90096f6b2b691e744d9a7
SZbytniewski <s.zbytniewski.624@studms.ug.edu.pl>
2023-12-18 15:26:20 +0100

    labt 11 prawie zrobione do zrobienia zostaly testy

--- app/RejestrKont.py
+++ app/RejestrKont.py
@@ -1,6 +1,10 @@
-from .Konto import Konto
+from .KontoOsobiste import KontoOsobiste
+from pymongo import MongoClient
 
 class RejestrKont:
+    client = MongoClient('localhost', 27017)
+    db = client['bank-app']
+    collection = db['konto']
     listaKont = []
 
     @classmethod
@@ -17,3 +21,20 @@ class RejestrKont:
     def ammount_of_accounts(cls):
         return len(cls.listaKont)
     
+    @classmethod
+    def save(cls):
+        cls.collection.delete_many({})
+        for konto in cls.listaKont:
+            cls.collection.insert_one(konto.__dict__)
+        return True
+
+    @classmethod
+    def load(cls):
+        cls.listaKont.clear()
+        for konto_data in cls.collection.find():
+            konto = KontoOsobiste(konto_data["imie"], konto_data["nazwisko"], konto_data["pesel"])
+            konto.history = konto_data["history"]
+            konto.saldo = konto_data["saldo"]
+            cls.listaKont.append(konto)
+        return True
+
